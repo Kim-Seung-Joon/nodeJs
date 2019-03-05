bd0e4c3f3efffe17f82c7c2812e27f6673a768bc
Kim-Seung-Joon <wldhel9228@gmail.com>
2019-03-05 19:10:37 +0900

    "디렉토리 만들기 및 파일 입출력시 읽는 파일과 출력되는 파일 업로드"

---
+++ chap4/4-8. mkdir_rmdir.js
@@ -0,0 +1,11 @@
+var fs = require("fs");
+
+fs.mkdir("./docs", 0666, err => {
+  if (err) throw err;
+  console.log("새로운 docs 폴더를 만들었습니다.");
+
+  fs.rmdir("./docs", err => {
+    if (err) throw err;
+    console.log("doc 폴더를 삭제했습니다");
+  });
+});
