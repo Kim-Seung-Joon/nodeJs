e4c6b667f8d002f5a70bff57d109d6fe1869e5f5
Kim-Seung-Joon <wldhel9228@gmail.com>
2019-03-26 17:01:09 +0900

    "exports에 참조할 수 없어서 결국에는 접근이 안되서 오류가 된다."

---
+++ chap7/module_test2.js
@@ -0,0 +1,10 @@
+var user = require('./user2');
+
+console.dir(user);
+
+
+function showUser() {
+  return user.getUser().name + ', '+ user.group().name;
+}
+
+console.log(showUser());
---
+++ chap7/user2.js
@@ -0,0 +1,8 @@
+exports = {
+  getUser: function() {
+    return {id: 'test01', name: '왕건'}
+  },
+  group: function() {
+    return {id: 'group01', name:'친구'}
+  }
+}
