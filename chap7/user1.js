b930548d720d0b487c670c69291b067e031fc6dc
Kim-Seung-Joon <wldhel9228@gmail.com>
2019-03-26 17:00:20 +0900

    "exports를 사용하여 호출하기"

---
+++ chap7/user1.js
@@ -0,0 +1,9 @@
+
+// exports 속성으로 유저의 아이디와 이름을 받는 getUser함수를 추가
+exports.getUser = function()  {
+  return {id: 'test01', name: '왕건'}
+}
+
+// exports 속성으로 마찬가지로 그룹을 추가
+exports.group = {id: 'group1', name: '친구'}
+
